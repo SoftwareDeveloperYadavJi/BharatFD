2c83154dbc710bf4a495364041745684e282812a
Nitin Yadav <108917474+SoftwareDeveloperYadavJi@users.noreply.github.com>
2025-01-31 23:06:21 +0530

    Added Admin Model

---
+++ src/models/adminModel.js
@@ -0,0 +1,19 @@
+import mongoose from "mongoose";
+
+// Define the schema
+const adminSchema = new mongoose.Schema(
+    {
+        username: { type: String, required: true },
+        password: { type: String, required: true },
+        email: { type: String, required: true },
+        role: { type: String, required: true },
+        isActive: { type: Boolean, default: true },
+        isvefified: { type: Boolean, default: false },
+        otp: { type: String, required: true },
+    },
+    { timestamps: true }
+);
+
+
+const Admin = mongoose.model("Admin", adminSchema);
+
---
+++ src/routes/adminRotes.js
@@ -0,0 +1,12 @@
+import express from "express";
+
+
+const adminRouter = express.Router();
+
+
+
+
+
+
+
+export default adminRouter;
